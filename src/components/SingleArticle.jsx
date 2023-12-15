75f9fe4f71d8ee65f25090756544fc3ec9ec4490
AnaBrh <anamariebrh@gmail.com>
2023-12-15 16:28:02 +0000

    css styling grid

--- src/components/SingleArticle.jsx
+++ src/components/SingleArticle.jsx
@@ -6,30 +6,31 @@ import { ListGroup } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 const SingleArticle = ({ article }) => {
-
 	const date = new Date(article.created_at);
 	const formattedDate = date.toDateString();
 
 	return (
-		<div className="overall-card-container">
-			<CardGroup>
-				<Card id="card-container">
+		<CardGroup className="overall-card-container">
+			<Card id="card-container">
+				<Card.Header>
 					<Card.Img variant="top" src={article.article_img_url} />
-					<Card.Body>
-						<Card.Title id="card-title">{article.title}</Card.Title>
-						<ListGroup id="card-list-group">
-							<Card.Text>Topic: {article.topic}</Card.Text>
-							<Card.Text>Comments: {article.comment_count}</Card.Text>
-							<Card.Text>Votes: {article.votes}</Card.Text>
-							<Card.Text>Created: {formattedDate}</Card.Text>
-						</ListGroup>
-						<Link to={`/articles/${article.article_id}`}>
-							<Button id="view-button">View Article</Button>
-						</Link>
-					</Card.Body>
-				</Card>
-			</CardGroup>
-		</div>
+				</Card.Header>
+				<Card.Body>
+					<Card.Title id="card-title">{article.title}</Card.Title>
+					<ListGroup id="card-list-group">
+						<Card.Text>Topic: {article.topic}</Card.Text>
+						<Card.Text>Comments: {article.comment_count}</Card.Text>
+						<Card.Text>Votes: {article.votes}</Card.Text>
+					</ListGroup>
+					<Link to={`/articles/${article.article_id}`}>
+						<Button id="view-button">View Article</Button>
+					</Link>
+					<Card.Footer id="card-footer">
+						<small>Created: {formattedDate}</small>
+					</Card.Footer>
+				</Card.Body>
+			</Card>
+		</CardGroup>
 	);
 };
 
